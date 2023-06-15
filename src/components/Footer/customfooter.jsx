f91e00ddc75d5e4d1112b8230db04a8bc0310931
orlocdiaz <luisorlandocarmonadiaz@gmail.com>
2023-06-15 02:09:49 -0600

    Navbar and Scroll animations

--- src/components/Footer/customfooter.jsx
+++ src/components/Footer/customfooter.jsx
@@ -5,15 +5,25 @@ const CustomFooter = () => {
   return (
     <footer className="footer-distributed">
       <div className="footer-right">
-        <a href="#"><img src="../public/instagram.png" alt="Instagram" /></a>
-        <a href="#"><img src="../public/facebook.png" alt="Facebook" /></a>
-        <a href="#"><img src="../public/twitter.png" alt="Twitter" /></a>
-        <a href="#"><img src="../public/linkedin.png" alt="LinkedIn" /></a>
+        <a href="#">
+          <img src="/instagram.png" alt="Instagram" />
+        </a>
+        <a href="#">
+          <img src="/facebook.png" alt="Facebook" />
+        </a>
+        <a href="#">
+          <img src="/twitter.png" alt="Twitter" />
+        </a>
+        <a href="#">
+          <img src="/linkedin.png" alt="LinkedIn" />
+        </a>
       </div>
 
       <div className="footer-left">
         <p className="footer-links">
-          <a className="link-1" href="#">Home</a>
+          <a className="link-1" href="#">
+            Home
+          </a>
           <a href="#">Blog</a>
           <a href="#">FAQ</a>
           <a href="#">Join the team</a>
