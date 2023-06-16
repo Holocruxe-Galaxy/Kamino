04cf3ba9d6c69fffa92462dc4857c00074b101ff
FerCazon <fernandocazon2@gmail.com>
2023-06-16 12:30:42 -0300

    cambios parciales

--- src/components/models/story.jsx
+++ src/components/models/story.jsx
@@ -1,48 +1,36 @@
-import React, { useRef, Suspense, useState } from "react";
-import { OrbitControls, useGLTF, Preload } from "@react-three/drei";
-import { useFrame, Canvas, useThree } from "@react-three/fiber";
-import * as THREE from "three";
+import React, { Suspense, useRef, useState } from "react";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { useFrame, Canvas } from "@react-three/fiber";
 import styles from './story.css?inline';
+import * as THREE from "three";
 
-const StoryModel = ({ scene, scale, position, mixer }) => {
+const StoryModel = () => {
+  const groupRef = useRef();
+  const { scene, animations } = useGLTF("./story/scene.gltf");
+  const [mixer] = useState(new THREE.AnimationMixer(scene));
+
+  animations.forEach((clip) => mixer.clipAction(clip).play());
+
   useFrame((state, delta) => mixer.update(delta));
   scene.rotation.x = Math.PI / 3;
+
   return (
-    <primitive
-      object={scene}
-      scale={scale}
-      position={position}
-    />
+    <group ref={groupRef}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2.5, 8, 5]} intensity={1} />
+      <primitive
+        object={scene}
+        scale={1}
+        position={[-1, 0, 0]}
+      />
+    </group>
   );
 };
 
-const StoryModelLoader = ({ modelPath, modelScale }) => {
-  const { scene, animations } = useGLTF(modelPath, undefined, undefined, (error) => {
-    console.error("An error occurred while loading the GLTF model:", error);
-  });
-  const initialized = useRef(false);
-  const [mixer, setMixer] = useState(null);
-
-  useThree(() => {
-    if (!initialized.current) {
-      const mix = new THREE.AnimationMixer(scene);
-      animations.forEach((clip) => mix.clipAction(clip).play());
-      setMixer(mix);
-      initialized.current = true;
-    }
-  });
-
-  return mixer ? <StoryModel scene={scene} scale={modelScale} position={[-1, 0, 0]} mixer={mixer} /> : null;
-};
-
-const StoryModelViewer = ({ modelPath, modelScale = 1 }) => {
+const StoryModelViewer = () => {
   return (
     <div className={styles.canvasContainer}>
-      <Canvas
-        style={{ width: '1200px', height: '1000px' }}
-      >
-        <directionalLight position={[2.5, 8, 5]} intensity={1} />
-        <directionalLight position={[-2.5, -8, -5]} intensity={0.7} />
+      <Canvas style={{ width: '1200px', height: '1000px' }}>
         <Suspense fallback={null}>
           <OrbitControls
             enableZoom={false}
@@ -50,7 +38,7 @@ const StoryModelViewer = ({ modelPath, modelScale = 1 }) => {
             minPolarAngle={Math.PI / 2}
           />
           <Preload all />
-          <StoryModelLoader modelPath={modelPath} modelScale={modelScale} />
+          <StoryModel />
         </Suspense>
       </Canvas>
     </div>
