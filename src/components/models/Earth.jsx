2fe644bf52b617a5f79e2cec5097592deaf2ed11
FerCazon <fernandocazon2@gmail.com>
2023-06-17 10:59:55 -0300

    no se pueden arrastrar los modelos y el logo lleva al home

--- src/components/models/Earth.jsx
+++ src/components/models/Earth.jsx
@@ -32,6 +32,7 @@ const EarthCanvas = () => {
     <Suspense fallback={<CanvasLoader />}>
       <OrbitControls
         enableZoom={false}
+        enablePan={false}
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
       />
--- src/components/models/modelviewer.jsx
+++ src/components/models/modelviewer.jsx
@@ -45,6 +45,7 @@ const ModelViewer = ({ modelPath, modelScale = 5 }) => {
         <Suspense fallback={null}>
           <OrbitControls
             enableZoom={false}
+            enablePan={false}
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}
           />
