4305162cb13003e4c0136b0c66c526070a53d8f9
FerCazon <fernandocazon2@gmail.com>
2023-06-21 11:48:48 -0300

    story,legacy

--- src/components/models/legacy.jsx
+++ src/components/models/legacy.jsx
@@ -3,27 +3,28 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import CanvasLoader from "./Loader"; 
 import { Canvas } from "@react-three/fiber";
+import styles from './legacy.css?inline';
 
 const LegacyModel = () => {
   const modelRef = useRef();
-  const model = useGLTF("./legacy/scene.gltf"); 
+  const model = useGLTF("./holocruxe/holocruxelogov2.gltf"); 
+ 
 
   useFrame(() => {
     if (modelRef.current) {
-      modelRef.current.rotation.y += 0.003; 
+      modelRef.current.rotation.y += 0.002; 
     }
   });
 
   return (
     <group>
-
     <ambientLight intensity={0.5} />
     <directionalLight position={[2.5, 8, 5]} intensity={1} />
     <primitive
       ref={modelRef}
       object={model.scene}
-      scale={0.6} 
-      position={[0, -3.78, 0]} 
+      scale={1} 
+      position={[-1.7, -0.1, 0]} 
       />
       </group>
   );
@@ -31,18 +32,20 @@ const LegacyModel = () => {
 
 const LegacyModelCanvas = () => {
   return (
-    <Canvas className="legacy-model-canvas" style={{ height: '500px' }}>
+        <div className={styles.canvasContainer}>
+
+    <Canvas className="legacy-model-canvas" style={{ width: '1200px', height: '700px' }}>
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          enablePan={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+      <OrbitControls
+  enableZoom={false}
+  enablePan={false}
+  maxPolarAngle={Math.PI / 2}
+  minPolarAngle={Math.PI / 2}/>
         <LegacyModel />
         <Preload all />
       </Suspense>
     </Canvas>
+  </div>
   );
 };
 
