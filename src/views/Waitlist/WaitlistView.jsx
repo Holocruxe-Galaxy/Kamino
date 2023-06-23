c8164e4aab851b7974db9a4cc53222f0718d09d4
orlocdiaz <luisorlandocarmonadiaz@gmail.com>
2023-06-23 01:11:50 -0600

    Waitlist view and about view  responsive

---
+++ src/views/Waitlist/WaitlistView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Waitlist from '../../components/Waitlist/waitlist';
+
+const WaitlistView = () => {
+  return (
+    <div>
+      <Waitlist />
+    </div>
+  );
+};
+
+export default WaitlistView;
