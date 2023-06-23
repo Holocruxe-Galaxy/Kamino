60fb838df3b9d5be808145a2b19391b4b213f442
orlocdiaz <luisorlandocarmonadiaz@gmail.com>
2023-06-23 09:53:37 -0600

    Reset to c8164e4 commit

--- src/views/Waitlist/WaitlistView.jsx
+++ src/views/Waitlist/WaitlistView.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import Waitlist from '../../components/Waitlist/waitlist';
+import Container from '../../components/Waitlist/waitlistcontainer';
+import styles from '../../components/Waitlist/waitlist.module.css';
+import Waitlist from '../../components/Waitlist/waitlist2';
+import { Canvas } from '@react-three/fiber';
+import Stars from '../../components/Stars/stars2';
 
 const WaitlistView = () => {
   return (
-    <div>
-      <Waitlist />
-    </div>
+    <section id="waitlist" className={styles.waitlist}>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Canvas
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Stars />
+        </Canvas>
+        <Waitlist />
+      </div>
+    </section>
   );
 };
 
