0bbd379f960110355249c01d5eea749d77d7db08
orlocdiaz <luisorlandocarmonadiaz@gmail.com>
2023-06-23 09:59:37 -0600

    Waitlist

--- src/components/Waitlist/waitlistcontainer.jsx
+++ src/components/Waitlist/waitlistcontainer.jsx
@@ -1,29 +1,28 @@
-<<<<<<< HEAD
-import LegacyModelCanvas from "../models/legacy";
-=======
->>>>>>> c6511cc03bf62dee21cee7e9139bdcd871a9cdf4
-import Waitlist from "./waitlist";
-import { Canvas } from "@react-three/fiber";
-import Stars from "../Stars/stars";
-
-
+import LegacyModelCanvas from '../models/legacy';
+import Waitlist from './waitlist';
+import { Canvas } from '@react-three/fiber';
+import Stars from '../Stars/stars';
 
 const Container = () => {
   return (
-    <div style={{
-      position: 'relative',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%',
-      height: '100%',
-    }}>
-      <Canvas style={{
-        position: 'absolute',
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
         width: '100%',
         height: '100%',
-      }}>
+      }}
+    >
+      <Canvas
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+      >
         <Stars />
       </Canvas>
       <Waitlist />
@@ -31,6 +30,4 @@ const Container = () => {
   );
 };
 
-export default Container;  
-
-  
+export default Container;
