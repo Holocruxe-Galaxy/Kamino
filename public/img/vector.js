c4668d4f8ab792f537f22cbade850ad40b406b01
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-09-11 10:55:13 -0300

    cambio de img de redes, imagenes nuevas y archivo js para la importacin de img

---
+++ public/img/vector.js
@@ -0,0 +1,41 @@
+import vector01 from './Vector 1.svg';
+import vector02 from './Vector 2.svg';
+import vector03 from './Vector 3.svg';
+import vector04 from './Vector 4.svg';
+import vector05 from './Vector 5.svg';
+import vector06 from './Vector 6.svg';
+import vector07 from './Vector 7.svg';
+import vector09 from './Vector 9.svg';
+import vector10 from './Vector 10.svg';
+import vector11 from './Vector 11.svg';
+import vector12 from './Vector 12.svg';
+import vector13 from './Vector 13.svg';
+import vector14 from './Vector 14.svg';
+import vector15 from './Vector 15.svg';
+import vector16 from './Vector 16.svg';
+import vector18 from './Vector 18.svg';
+import vector19 from './Vector 19.svg';
+import vector20 from './Vector 20.svg';
+
+let vector = {
+  vector01,
+  vector02,
+  vector03,
+  vector04,
+  vector05,
+  vector06,
+  vector07,
+  vector09,
+  vector10,
+  vector11,
+  vector12,
+  vector13,
+  vector14,
+  vector15,
+  vector16,
+  vector18,
+  vector19,
+  vector20,
+}
+
+export default vector;
