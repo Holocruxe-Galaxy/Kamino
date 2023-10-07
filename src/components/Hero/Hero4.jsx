a82be06f485afd2ddcc9052d39e3f3f7f77816fc
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-10-06 22:17:17 -0300

    refactor(client/Hero/*): Fix/update responsibe para tamaño tablet y celular, eliminacion de código repetido, css actualizado

--- src/components/Hero/Hero4.jsx
+++ src/components/Hero/Hero4.jsx
@@ -1,10 +1,10 @@
-import styles from "./heroTexto.module.css";
+import styles from "./hero.module.css";
 
 const Hero4 = () => {
   return (
-    <section className={styles.hero_container}>
+    <section className={`${styles.hero_container} ${styles.heroText}`}>
         
-      <div className={styles.hero}>
+      <div className={`${styles.hero} ${styles.heroP}`}>
         <h2>Do you want<span className={styles.title}> to know more?</span></h2>
         <p>
         Join our waiting list and be one of the first people to try this unique experience.
