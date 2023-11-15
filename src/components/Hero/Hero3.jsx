f0a1841a54998f711d136a64481f79fed0024461
Altus24 <116025673+Altus24@users.noreply.github.com>
2023-11-15 17:06:19 -0300

    textos alineados del home

--- src/components/Hero/Hero3.jsx
+++ src/components/Hero/Hero3.jsx
@@ -7,12 +7,12 @@ const Hero3 = () => {
         <div className={`${styles.hero_img} ${styles.Hero_imgBottom}`}>
           <img className={styles.imgAcostado} src={robot.rob03} alt="Robot acostado" />
         </div>
-      <div className={styles.hero}>
+        <div className={styles.hero}>
         <h2>All in <span className={styles.title}> one place</span></h2>
         <p className={styles.heroPBottom}>
         Imagine never again losing an important memory or wasting time searching for information scattered across different platforms. Our Holocruxe manages your data efficiently and securely, allowing you to relive moments and access crucial information with ease.
         </p>
-        
+
       </div>
     </section>
   );
