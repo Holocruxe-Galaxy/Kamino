885a9d785dc76f58fc69dba7f47c013d4702bef3
Altus24 <116025673+Altus24@users.noreply.github.com>
2023-11-27 19:04:23 -0300

    fix(src/components/Footer): Add onClick to go to the top of the page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,57 +1,59 @@
-import styles from './Footer.module.css';
-import { NavLink } from 'react-router-dom';
-import redes from '../../img/redes.js'
+import styles from "./Footer.module.css";
+import { NavLink } from "react-router-dom";
+import redes from "../../img/redes.js";
 
 const Footer = () => {
   return (
     <>
-    <div className={styles.separator}>
-      
-    </div>
-    <footer className={styles.footerDistributed}>
-      <div className={styles.footerLeft}>
-        <img src="./holocruxe.png" alt='logo' />
-      </div>
+      <div className={styles.separator}></div>
+      <footer className={styles.footerDistributed}>
+        <div className={styles.footerLeft}>
+          <img src="./holocruxe.png" alt="logo" />
+        </div>
 
-      <div className={styles.footerCenter}>
-        <p className={styles.footerLinks}>
-          
+        <div className={styles.footerCenter}>
+          <p className={styles.footerLinks}>
             <NavLink
-          to="/"  className={`${styles.link1} ${styles.navlink} ${
-            location.pathname === '/' && styles.activeLink
-          }`}
-        >
-          Home
-        </NavLink>
-          
+              to="/"
+              className={`${styles.link1} ${styles.navlink} ${
+                location.pathname === "/" && styles.activeLink
+              }`}
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              Home
+            </NavLink>
 
-          <NavLink to="/faqs" className={styles.navlink}>
-            FAQ
-          </NavLink>
-        </p>
-        <p className={styles.copy}>Holocruxe &copy; 2023</p>
-      </div>
+            <NavLink
+              to="/faqs"
+              className={styles.navlink}
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              FAQ
+            </NavLink>
+          </p>
+          <p className={styles.copy}>Holocruxe &copy; 2023</p>
+        </div>
 
-      <div className={styles.footerRight}>
-        <a
-          href="https://www.instagram.com/holocruxe/"
-          className={styles.navlink}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={redes.instagram} alt='instagram' />
-        </a>
-        <a
-          href="https://www.linkedin.com/company/holocruxe/"
-          className={styles.navlink}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={redes.linkedin} alt="LinkedIn" />
-        </a>
-      </div>
-      <p className={styles.copyMobile}>Holocruxe &copy; 2023</p>
-    </footer>
+        <div className={styles.footerRight}>
+          <a
+            href="https://www.instagram.com/holocruxe/"
+            className={styles.navlink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={redes.instagram} alt="instagram" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/holocruxe/"
+            className={styles.navlink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={redes.linkedin} alt="LinkedIn" />
+          </a>
+        </div>
+        <p className={styles.copyMobile}>Holocruxe &copy; 2023</p>
+      </footer>
     </>
   );
 };
