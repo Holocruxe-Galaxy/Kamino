456b16aea54166f1b0d468e1e60c98b8ca602231
acroquer <acroquer@quo.team>
2025-06-13 15:24:43 -0300

    refactor: clean up code formatting and improve readability across multiple components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
 import styles from "./Footer.module.css";
-import { IoMailOutline } from "react-icons/io5";
-import { FaInstagram } from "react-icons/fa";
-import { TbBrandLinkedin } from "react-icons/tb";
+import {IoMailOutline} from "react-icons/io5";
+import {FaInstagram} from "react-icons/fa";
+import {TbBrandLinkedin} from "react-icons/tb";
 
 const Footer = () => {
   return (
     <>
       <footer className={styles.footerDistributed}>
         <div className={styles.footerLeft}>
-          <img src="./holocruxe.png" alt="logo" />
+          <img src='./holocruxe.png' alt='logo' />
         </div>
 
         <div className={styles.footerCenter}>
@@ -16,25 +16,25 @@ const Footer = () => {
         </div>
 
         <div className={styles.footerRight}>
-          <a
+          {/* <a
             href="mailto:contacto@holocruxe.com" 
             className={styles.navlink}
           >
             <IoMailOutline className={styles.socialIcon} />
-          </a>
+          </a> */}
           <a
-            href="https://www.instagram.com/holocruxe/"
+            href='https://www.instagram.com/holocruxe/'
             className={styles.navlink}
-            target="_blank"
-            rel="noreferrer"
+            target='_blank'
+            rel='noreferrer'
           >
             <FaInstagram className={styles.socialIcon} />
           </a>
           <a
-            href="https://www.linkedin.com/company/holocruxe/"
+            href='https://www.linkedin.com/company/holocruxe/'
             className={styles.navlink}
-            target="_blank"
-            rel="noreferrer"
+            target='_blank'
+            rel='noreferrer'
           >
             <TbBrandLinkedin className={styles.socialIcon} />
           </a>
